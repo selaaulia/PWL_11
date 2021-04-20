03fd7f27e401bcf38fdda9235e13bcaa507d282f
selaaulia <selaaulia1307@gmail.com>
2021-04-20 19:56:12 +0700

    Menambahkan fungsi render untuk memastikan bahwa respon dikembalikan dalam format API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -38,4 +40,17 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json(
+                ['message' => 'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
+    }
+
 }
