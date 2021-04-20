2cd11532471449c320bb6a0e08e9c91ace8b7790
selaaulia <selaaulia1307@gmail.com>
2021-04-20 15:49:03 +0700

    Menambhakan Trait ApiResponse

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponse;
 }
